edb2a69a8d5b32f56dc55cc1aaaa6a8053f48a9d
miguelfiais <miguel.fiais10@gmail.com>
2023-08-06 12:45:29 -0300

    feat: add user cover photo preview

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import ItemPost from "../components/ItemPost";
@@ -43,6 +44,29 @@ const MyPosts = () => {
 
   return (
     <div className="bg-gray-100 h-[calc(100vh-74.29px)] overflow-y-auto">
+      <div className="relative w-full h-1/5">
+        <Image
+          src="/capa.png"
+          alt="foto de capa"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="container mx-auto p-5 -mt-10 relative flex items-center gap-3 pb-0">
+        <Image
+          src={data?.user?.image!}
+          alt={data?.user?.name!}
+          width={120}
+          height={120}
+          className="rounded-full border-2 border-blue-400"
+        />
+        <div>
+          <p className="font-bold text-gray-700 text-lg">{data?.user?.name}</p>
+          <p>
+            <p className="text-gray-500">{posts.length} Publicações</p>
+          </p>
+        </div>
+      </div>
       <div className="p-5">
         {posts.length > 0 &&
           posts.map((post) => <ItemPost data={post} key={post.id} />)}
--- src/app/post/user/[userId]/page.tsx
+++ src/app/post/user/[userId]/page.tsx
@@ -1,5 +1,6 @@
 import ItemPost from "@/app/components/ItemPost";
 import { prisma } from "@/lib/prisma";
+import Image from "next/image";
 
 const fetchUserPosts = async (userId: string) => {
   const userPosts = await prisma.post.findMany({
@@ -25,6 +26,29 @@ const UserPosts = async ({ params }: { params: { userId: string } }) => {
 
   return (
     <div className="bg-gray-100 h-[calc(100vh-74.29px)] overflow-y-auto">
+      <div className="relative w-full h-1/5">
+        <Image
+          src="/capa.png"
+          alt="foto de capa"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="container mx-auto p-5 -mt-10 relative flex items-center gap-3 pb-0">
+        <Image
+          src={data[0].User.image!}
+          alt={data[0].User.name!}
+          width={120}
+          height={120}
+          className="rounded-full border-2 border-blue-400"
+        />
+        <div>
+          <p className="font-bold text-gray-700 text-lg">
+            {data[0].User.name!}
+          </p>
+          <p className="text-gray-500">{data.length} Publicações</p>
+        </div>
+      </div>
       <div className="p-5">
         {data.map((post) => (
           <ItemPost key={post.id} data={post} />
